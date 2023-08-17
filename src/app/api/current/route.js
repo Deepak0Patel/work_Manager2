a00104ecd4d6ccf152b23aba97b1d8965a7d2298
Deepak  Patel <138191114+Deepak0Patel@users.noreply.github.com>
2023-08-17 14:03:25 +0530

    Update route.js

--- src/app/api/current/route.js
+++ src/app/api/current/route.js
@@ -1,18 +1,18 @@
-import { NextResponse } from "next/server";
-// import jwt from "jsonwebtoken";
-import { User } from "@/models/user";
-const jwt = require("jsonwebtoken");
-export async function GET(request) {
-  try {
-    const authToken = request.cookies.get("authToken")?.value;
-    const data = jwt.verify(authToken, process.env.JWT_KEY);
-    const user = await User.findById(data._id).select("-password");
-    return NextResponse.json(user);
-  } catch (error) {
-    // Handle JWT verification errors
-    console.error("JWT verification error:", error);
-    return NextResponse.error("Unauthorized", { status: 401 });
-  }
+// import { NextResponse } from "next/server";
+// // import jwt from "jsonwebtoken";
+// import { User } from "@/models/user";
+// const jwt = require("jsonwebtoken");
+// export async function GET(request) {
+//   try {
+//     const authToken = request.cookies.get("authToken")?.value;
+//     const data = jwt.verify(authToken, process.env.JWT_KEY);
+//     const user = await User.findById(data._id).select("-password");
+//     return NextResponse.json(user);
+//   } catch (error) {
+//     // Handle JWT verification errors
+//     console.error("JWT verification error:", error);
+//     return NextResponse.error("Unauthorized", { status: 401 });
+//   }
 
   // const authToken = request.cookies.get("authToken")?.value;
   // console.log(authToken);
